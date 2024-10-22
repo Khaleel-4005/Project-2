1531bb6237686bbae555d5894f9b3a77f6142961
Khaleel-4005 <khaleel4005@gmail.com>
2024-10-22 17:26:56 +0530

    madhu

---
+++ JavaScript3/third.js
@@ -0,0 +1,223 @@
+
+// By using var
+
+// var age=24;
+// var age=35;
+// var age=67;
+// console.log(age);
+
+
+// Here by using let 
+
+// let age=34;
+// age=56;
+// age=98
+// console.log(age);
+
+
+
+// Here by using const
+
+// const PI =3.14;
+// console.log(PI);
+
+
+// when we can use const intiaizse the value 1st
+
+// const k=10;
+// console.log(k);
+
+
+// let used in block only for example:
+
+// {
+//     let a= 10;
+//     console.log(a);
+// }
+// {
+//     let a= 15;
+//     console.log(a);
+// }
+
+
+// data types in primitive 
+// NUMBER data types
+
+// let age=24;
+
+// STRING data type
+
+// let fullName = "robot";
+
+// BOOLEN data type
+
+// isFollow=true;
+
+// UNDEFINED data type
+
+// let x;
+
+// NULL DATA TYPE
+// let x= null;
+
+// BigInt data type
+// let x=BigInt("123");
+
+
+// Symbol data type
+
+// let k=Symbol("hai!");
+
+
+// OBJECT enter data of a student
+
+// const student={
+//     fullName :"Khaleel",
+//     age:24,
+//     cgpa:8.1,
+//     ispass:true  
+// };
+
+// // or
+
+// console.log(student["fullName"]);
+
+// student["name"]="khaleel icon";
+
+// console.log(student["name"]);
+
+
+
+// create a const object called "product" to store information in the picture
+
+
+// const product={
+//     name:"ball pen",
+//     rating:4,
+//     price:300,
+//     offer:5
+// };
+// console.log(product);
+
+
+// create a const object called profile to store information shown in the picture
+  
+
+// const profile={
+//     username:"@khaleelicon",
+//     followers:800,
+//     following:2,
+//     isfollow:true,
+// };
+
+
+
+
+// Arithimatic Opraters
+
+// let a =2;
+// let b=3;
+
+// console.log("a=",a , "& b=",b);
+// console.log("a+b",a+b);
+// console.log("a-b",a-b);
+// console.log("a*b",a*b);
+// console.log("a/b",a/b);
+// console.log("a%b",a%b);
+// console.log("a**b",a**b);
+
+
+// Unary Operator
+
+// let a =2;
+// let b=3;
+// console.log("a=",a , "& b=",b);
+// // a++;
+// // a=a+1;
+// // a--;
+// // a= a-1;
+// console.log("a=",a);
+
+// Assignment operators:
+
+// =  +=   -=  *=  %=  **=
+
+// let a=2;
+// let b=3;
+// // a += 2;
+// // a -= 4;
+// // a *= 4;
+// // a /= 4;
+// a ** 4;
+// console.log("a=",a); //a=a+2//a=4
+
+
+// comparsion operators:
+
+// equal to == ,    equal to & type ===
+// Not equal to !=,  Not equal to & type !==
+
+// let a = 5;
+// let b = 2;
+// console.log("5 == 2", a==b);
+// console.log("5 === 2", a===b);
+
+// let a = 5;
+// let b = 5;
+// // console.log("5 != 5", a != b);
+// console.log("5 !== 5", a !== b);
+
+// let a=5;
+// let b=3;
+// // // console.log("5 > 3",a > b);
+// // // console.log("5 > 3",a<b);
+// // // console.log(5 <= 3,a <= b);
+// console.log(5 >= 3,a >= b);
+
+// logical operators;
+
+// AND &&
+// OR ||
+// NOT !
+
+// let a=6;
+// let b=5;
+// let cond1 = a > b;
+// let cond2 = a === 5;
+// console.log("cond1 && cond2=", cond1 && cond2);
+
+// let a=6;
+// let b=5;
+// console.log("cond1 || cond2=",a>b || a ===b);
+
+// let a=6;
+// let b=5;
+// console.log("!(6<5)=",!(a<b));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
